dc9705e0883fc61560d3293d0d24ee90982afb50
Юлия <jullka5@mail.ru>
2023-01-25 21:19:51 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = Math.pow(b, 2) - (4 * a * c);
+  let x = -b / (2 * a);
+  let x1 = (-b + Math.sqrt(D)) / (2 * a);
+  let x2 = (-b - Math.sqrt(D)) / (2 * a);
+
+  if (D < 0) {
+  } else if (D == 0) {
+    arr = [x]; 
+  } else if (D > 0) {
+    arr = [x1, x2];
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  let S = amount - contribution;
+  let P  = percent / 100 / 12;
+  let n = countMonths;
+
+  if ((percent < 0 ||  contribution < 0 || amount < 0 || n < 0 ) || 
+  (isNaN(percent) === true|| isNaN(contribution) === true || isNaN(amount) === true || isNaN(countMonths) === true)) {
+  return false;
+  }
+
+  let payment = S * (P + (P / (((1 + P) ** n) - 1)));
+  let totalAmount = (payment * n).toFixed(2);
+
+  console.log(totalAmount);
+  return +totalAmount;
+}  
