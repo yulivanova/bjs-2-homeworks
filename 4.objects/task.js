5a49b3fc8dc1f166798807a45b9b3bdd20d60094
Юлия <jullka5@mail.ru>
2023-02-08 15:18:33 +0300

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  return this.marks ? this.marks.push(...marks) : 'студент отчислен';
 }
 
 Student.prototype.getAverage = function () {
-  
+  return this.marks && this.marks.length ? this.marks.reduce((acc, cur) => acc + cur, 0) / this.marks.length : 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
+
+let student1 = new Student("Tony", "male", 37);
+student1.setSubject("Algebra");
+student1.addMark(5);
+student1.addMark(4);
+student1.addMark(5);
+console.log(student1.getAverage());
+console.log(student1);
+
+let student2 = new Student("Buzz", "female", 35);
+student2.setSubject("Geometry");
+student2.addMark(2);
+student2.addMark(3);
+student2.addMark(2);
+student2.exclude('low grades')
+console.log(student2)
